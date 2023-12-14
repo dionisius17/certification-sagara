668ad4dee912e4b0cfdcde29f97a5caac242b971
unknown <dionisiusbrisko@gmail.com>
2023-12-14 15:53:31 +0700

    firstt commit

---
+++ accounts/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'usernamace', 'email')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs= {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(validated_data['username'],validated_data['email'],validated_data['password'])
+
+        return user
+    
+class ChangePasswordSerializer(serializers.Serializer):
+    model = User
+    # Serializer untuk password menggantikan endpoint.
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
